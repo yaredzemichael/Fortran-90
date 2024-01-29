89a6da3a6bffd30a7204ce39fa24ca1a83e32333
yaredzemichael <yaredzemichael@gmail.com>
2024-01-30 03:46:25 +0530

    mult1

---
+++ multi/mult1.f90
@@ -0,0 +1,13 @@
+programe mult1
+implicit none
+integer:: i,j,k
+!
+!This fortran program is the multiplication of two integer 
+!finaly display the producte of these interger 
+!
+i=8
+j=5
+k=i*j
+write(*,*)i,j,k
+stop
+end programe mult1
